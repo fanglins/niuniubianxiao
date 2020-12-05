7f05cfd0839278b3fd4b67a07256078adbd5c32b
fanglins <995008651@qq.com>
2020-12-05 21:50:10 +0800

    test

---
+++ nooner/src/main/java/zwq/geo/onner/OnnerApplication.java
@@ -0,0 +1,13 @@
+package zwq.geo.onner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnnerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnnerApplication.class, args);
+    }
+
+}
---
+++ nooner/src/test/java/zwq/geo/onner/OnnerApplicationTests.java
@@ -0,0 +1,13 @@
+package zwq.geo.onner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnnerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
